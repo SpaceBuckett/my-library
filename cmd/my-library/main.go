fb2786cde471866d4db96883487476a61c1a83d5
echo-shahzad <shehxadv2.0@gmail.com>
2024-11-24 15:06:49 +0500

    project setup

---
+++ cmd/my-library/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("WELCOME TO SHAHZAD'S LIBRARY")
+	fmt.Println("----------------------------")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/SpaceBuckett/books-api
+
+go 1.23.2
